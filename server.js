3ea6bb787c18f1d3c170c81d06cf737c2514aed3
Noah Aron <noahmojo@yahoo.com>
2026-03-27 08:02:58 -1000

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 app.listen(PORT, () => {
