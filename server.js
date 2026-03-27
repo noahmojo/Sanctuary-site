6124852349e8055adfbb336ada65f45ebac0d139
Noah Aron <noahmojo@yahoo.com>
2026-03-27 07:57:01 -1000

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(__dirname));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
